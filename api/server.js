a4bd8b2ab0b1f7a3c11948fb7582f6f958c82d8d
mcmerdanoglu <mcmerdanoglu@gmail.com>
2023-05-15 17:17:20 +0300

    Derse kadar olan bölüm

--- api/server.js
+++ api/server.js
@@ -1,3 +1,103 @@
 // SUNUCUYU BU DOSYAYA KURUN
 
-module.exports = {}; // SERVERINIZI EXPORT EDİN {}
+const express = require("express");
+
+const Users = require("./users/model");
+
+const server = express();
+
+server.use(express.json());
+
+server.get("/api/users", (req, res) => {
+  Users.find()
+    .then((users) => {
+      res.json(users);
+    })
+    .catch((error) => {
+      res.status(500).json({
+        message: "Kullanıcı bilgisine ulaşılamıyor.",
+      });
+    });
+});
+
+server.get("/api/users/:id", (req, res) => {
+  Users.findById(req.params.id)
+    .then((user) => {
+      if (user === undefined) {
+        res.status(404).json({
+          message: "Girilen id ile eşleşen kullanıcı bulunamadı.",
+        });
+      }
+      res.json(user);
+    })
+    .catch((error) => {
+      res.status(500).json({
+        message: "Kullanıcı bilgisine ulaşılamıyor.",
+      });
+    });
+});
+
+server.post("/api/users", (req, res) => {
+  const user = req.body;
+  if (!user.name || !user.bio) {
+    res.status(400).json({
+      message: "Lütfen kullanıcı için bir name ve bio sa girin.",
+    });
+  } else {
+    Users.insert(user)
+      .then((newUser) => {
+        res.status(201).json(newUser);
+      })
+      .catch((error) => {
+        res.status(500).json({
+          message: "Kullanıcıyı veritabanına kaydederken bir hata oluştu",
+        });
+      });
+  }
+});
+
+server.delete("/api/users/:id", async (req, res) => {
+  const possibleUser = await Users.findById(req.params.id);
+  if (!possibleUser) {
+    res.status(404).json({
+      message: "Girilen id ile eşleşen kullanıcı bulunamadı.",
+    });
+  } else {
+    Users.remove(possibleUser.id)
+      .then((deletedUser) => {
+        res.json(deletedUser);
+      })
+      .catch((error) => {
+        res.status(500).json({
+          message: "Kullanıcı silinemedi.",
+        });
+      });
+  }
+});
+
+server.put("/api/users/:id", async (req, res) => {
+  const possibleUser = await Users.findById(req.params.id);
+  if (!possibleUser) {
+    res.status(404).json({
+      message: "Girilen id ile eşleşen kullanıcı bulunamadı.",
+    });
+  } else {
+    if (!req.body.name || !req.body.bio) {
+      res.status(400).json({
+        message: "Lütfen kullanıcı name ve biosu girin.",
+      });
+    } else {
+      Users.update(req.params.id, req.body)
+        .then((updatedUser) => {
+          res.status(200).json(updatedUser);
+        })
+        .catch((error) => {
+          res.status(500).json({
+            message: "Kullanıcı bilgileri güncellenemedi!",
+          });
+        });
+    }
+  }
+});
+
+module.exports = server; /*{};*/ // SERVERINIZI EXPORT EDİN {}
